7014942e68b8ef9989f83efe58f00e2282fdfa08
Hammad <official.hammadansari@gmail.com>
2025-03-15 14:09:45 +0545

    6th commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,7 +1,7 @@
 import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
-export default serve({
+export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
